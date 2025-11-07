0f86e73f9359c95823681d16f6cbd54aab79e5d7
Stef <stefus.sp@gmail.com>
2025-11-07 10:36:55 +0100

    fetch con codigo compartido en productos

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
+// --- Menú móvil ---
 document.querySelector('.menu-toggle').addEventListener('click', function () {
   const mobileMenu = document.querySelector('.mobile-menu');
   mobileMenu.classList.toggle('active');
 });
+
